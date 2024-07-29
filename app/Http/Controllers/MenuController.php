5b0a97c5e42dece7f93602574c99da355e9755e3
SonHai4101 <91021500+SonHai4101@users.noreply.github.com>
2024-07-29 12:50:00 +0700

    minor

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -4,14 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Services\Menu\MenuService;
+use Illuminate\Support\Facades\Session;
+use App\Http\Services\CartService;
 
 class MenuController extends Controller
 {
     protected $menuService;
+    protected$cart;
 
-    public function __construct(MenuService $menuService)
+    public function __construct(MenuService $menuService, CartService $cart)
     {
         $this->menuService = $menuService;
+        $this->cart = $cart;
     }
 
     public function index(Request $request, $id, $slug = '')
@@ -22,7 +26,8 @@ class MenuController extends Controller
         return view('menu', [
             'title' => $menu->name,
             'products' => $products,
-            'menu' => $menu
+            'menu' => $menu,
+            'carts' => Session::get('carts')
         ]);
     }
 }
--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,22 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Services\Product\ProductService;
+use App\Http\Services\CartService;
+use Illuminate\Support\Facades\Session;
+
 
 class ProductController extends Controller
 {
     protected $productService;
+    protected $cart;
 
-    public function __construct(ProductService $productService) {
-            $this->productService = $productService;
-        }
-    public function index($id = '', $slug = '') {
+    public function __construct(ProductService $productService, CartService $cart)
+    {
+        $this->productService = $productService;
+        $this->cart = $cart;
+    }
+    public function index($id = '', $slug = '')
+    {
         $product = $this->productService->show($id);
         $productsMore = $this->productService->more($id);
 
         return view('products.content', [
             'title' => $product->name,
             'product' => $product,
-            'products' => $productsMore
+            'products' => $productsMore,
+            'carts' => Session::get('carts')
         ]);
     }
 }
